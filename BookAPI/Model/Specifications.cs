ae383ce78378c01b25914d3a931aa48555b49838
Elcio Cestari Taira <elciotaira@gmail.com>
2020-10-16 16:51:31 -0400

    corrigido exibicao de originally published

--- BookAPI/Model/Specifications.cs
+++ BookAPI/Model/Specifications.cs
@@ -1,13 +1,15 @@
 ﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace BookAPI.Model
 {
     public class Specifications
     {
-        public String Originally_published { get; set; }
+        [JsonPropertyName("Originally published")]
+        public String Originallypublished { get; set; }
         public string Author { get; set; }
         public int Pagecount { get; set; }
         public object Illustrator { get; set; }
